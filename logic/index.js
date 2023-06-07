48bdf8bd1fc07fc5aefb9e77385378254fa1f302
amankanz <akanezamulume.iuea@gmail.com>
2023-06-07 19:10:56 +0300

    softcodes-v3

--- logic/index.js
+++ logic/index.js
@@ -1,63 +1,17 @@
-const boxes = document.querySelectorAll('.box');
-    let images = [
-      './images/img-1.JPG',
-      './images/img-2.JPG',
-      './images/img-3.JPG',
-      './images/img-4.JPG',
-      './images/img-5.JPG',
-      './images/img-6.JPG',
-      './images/img-7.JPG',
-      './images/img-8.JPG',
-      // Add more image paths as needed
-    ];
-    const interval = 3000; // Change image every 3 seconds
+window.addEventListener('load', () => {
+  const boxes = document.querySelectorAll('.box');
+  const animationDuration = 4000;
 
-    function getRandomImage() {
-      const randomIndex = Math.floor(Math.random() * images.length);
-      const selectedImage = images[randomIndex];
-      images.splice(randomIndex, 1); // Remove selected image from the array
-      return selectedImage;
-    }
+  let currentIndex = 0;
 
-    function fadeOutImage(box) {
-      box.style.transition = 'opacity .3s'; // Set the transition property for the fading effect
-      box.style.opacity = '0.7'; // Fade out the box
-    }
+  function showNextBox() {
+    boxes[currentIndex].style.transform = 'translateX(-100%)';
+    currentIndex = (currentIndex + 1) % boxes.length;
+    boxes[currentIndex].style.transform = 'translateX(0)';
+  }
 
-    function fadeInImage(box, newImage) {
-      box.style.transition = 'opacity .3s'; // Set the transition property for the fading effect
-      box.style.backgroundImage = `url(${newImage})`;
-      box.style.opacity = '1'; // Fade in the new image
-    }
-
-    function changeBackgroundImage(box) {
-      const newImage = getRandomImage();
-      fadeOutImage(box);
-      setTimeout(() => {
-        fadeInImage(box, newImage);
-      }, Math.random() * 2000 + 1000); // Random delay between 1s and 3s for each box
-    }
-
-    function rotateBackgroundImages() {
-      boxes.forEach((box) => {
-        setInterval(() => {
-          if (images.length === 0) {
-            // Reset the images array if all images have been used
-            images = [
-              './images/img-1.JPG',
-              './images/img-2.JPG',
-              './images/img-3.JPG',
-              './images/img-4.JPG',
-              './images/img-5.JPG',
-              './images/img-6.JPG',
-              './images/img-7.JPG',
-              './images/img-8.JPG',
-              // Add more image paths as needed
-            ];
-          }
-          changeBackgroundImage(box);
-        }, interval);
-      });
-    }
-
-    rotateBackgroundImages();
+  setTimeout(() => {
+    showNextBox(); // Start the initial sliding animation
+    setInterval(showNextBox, animationDuration); // Schedule subsequent sliding animations
+  }, 0.3); // Delay the start of the sliding animation by 1 second
+});
